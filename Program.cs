3376d2e0a8c52d6c75c147f10427636452bbb3ad
anilac13 <anil4u2496@gmail.com>
2023-04-25 08:19:19 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 'd';
+            DemoApp demo = new DemoApp();
+            demo.Add();
+        }
+    }
+}
